f9aada3222c90022b4e609fc196d106a8fd817d2
billryan <yuanbin2014@gmail.com>
2015-06-01 20:35:53 +0800

    update ef_gen with ef_template

--- scripts/ef_gen.py
+++ scripts/ef_gen.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 import os
-from sys import argv
+import sys
 import re
 
 # obtain packet arrival rate list
@@ -21,14 +21,14 @@ runsim_header = '''#!/usr/bin/sh
 def runsim_from_ef(ef, seed):
     ef_real = os.path.basename(ef)
     net_name = ef_real[:ef_real.find('-DES-')]
-    ef_prefix = net_name + '-' + seed + '-DES-'
+    ef_prefix = net_name + '_' + seed + '-DES-'
     runsim_dir = "runsim_src/"
     runsim_fn = runsim_dir + "runsim_" + net_name + '-' + seed + '.sh'
     with open(runsim_fn, 'w') as runsim_fp:
         runsim_fp.writelines(runsim_header)
     for i, item in enumerate(msdu_interval):
         ef_fn = 'p' + ef_prefix + format(i + 1, '02d') + '.ef'
-        ef_fn = os.path.join('ef_src', ef_fn)
+        ef_path = os.path.join('ef_src', ef_fn)
         runsim_line = 'op_runsim -net_name ' + net_name + \
             ' -noprompt -m32 -ef ' + ef_fn[:-3] + \
             ' -DESinfo ' + ef_fn[:-3] + \
@@ -36,7 +36,7 @@ def runsim_from_ef(ef, seed):
         with open(runsim_fn, 'a') as runsim_fp:
             runsim_fp.writelines(runsim_line)
         # write ef_new with ef_tml
-        with open(ef, 'U') as ef_tml, open(ef_fn, 'w') as ef_new:
+        with open(ef, 'U') as ef_tml, open(ef_path, 'w') as ef_new:
             for line in ef_tml:
                 # replace log_file, ot_file, ov_file
                 if re.search('^\".*_file\"', line):
@@ -63,7 +63,10 @@ def runsim_from_ef(ef, seed):
                 ef_new.writelines(line)
 
 if __name__ == "__main__":
-    ef_files = argv[1:]
+    if len(sys.argv) < 2:
+        print("Usage: python %s <ef_template>") % sys.argv[0]
+        sys.exit(1)
+    ef_files = sys.argv[1:]
     for ef_file in ef_files:
         for seed in random_seed:
             runsim_from_ef(ef_file, seed)
