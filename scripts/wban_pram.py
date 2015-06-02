77cebf109a2ad3b1c8d452bd90ae3a089acd4fb6
billryan <yuanbin2014@gmail.com>
2015-06-02 23:36:34 +0800

    add wban_pram for constant

---
+++ scripts/wban_pram.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# user priority of packet
+UP0 = 0
+UP1 = 1
+UP2 = 2
+UP3 = 3
+UP4 = 4
+UP5 = 5
+UP6 = 6
+UP7 = 7
+# data State - Generate, Queue Success, Sent, Receive
+GEN = 0
+Q_SUCC = 1
+Q_FAIL = 2
+SENT = 3
+RCV = 4
+SUBQ = 5
+FAIL = 6
+
+# Max number of BAN coexist
+MAX_BAN_NET = 16
